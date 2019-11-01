21ee6087493d43e281f292730c94617e5943be82
pengzhe <zhe.peng@alibaba-inc.com>
2019-11-01 16:50:21 +0800

    test pull request

---
+++ module1/src/main/java/yang/java/test/TestPullRequest.java
@@ -0,0 +1,11 @@
+package yang.java.test;
+
+/**
+ * @author pengzhe
+ * @date 2019/11/1 16:48
+ */
+public class TestPullRequest {
+    public static void main(String[] args) {
+        System.out.println(TestPullRequest.class.getCanonicalName());
+    }
+}
